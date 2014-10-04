ce2ee80af9a12c436715a1f7849a59d0a3f4d65a
Liezl Puzon <liezl.200@gmail.com>
2014-10-04 05:53:40 -0700

    Don't load the tagger in order to make load time much faster

--- src/SUTimeDemo.java
+++ src/SUTimeDemo.java
@@ -22,24 +22,24 @@ public class SUTimeDemo {
     pipeline.addAnnotator(new WordsToSentencesAnnotator(false));
     
     String modelDir = "models/";
-    //MaxentTagger tagger1 = new MaxentTagger(modelDir + "src/english-bidirectional-distsim.tagger");
-    MaxentTagger tagger = new MaxentTagger(modelDir + "pos-tagger/english-bidirectional/english-bidirectional-distsim.tagger");
-    pipeline.addAnnotator(new POSTaggerAnnotator(tagger));
+
+    //MaxentTagger tagger = new MaxentTagger(modelDir + "pos-tagger/english-bidirectional/english-bidirectional-distsim.tagger");
+    //pipeline.addAnnotator(new POSTaggerAnnotator(tagger));
     
     String sutimeRules = modelDir + "/sutime/defs.sutime.txt,"
-    		+ modelDir + "/sutime/english.holidays.sutime.txt,"
-    		+ modelDir + "/sutime/english.sutime.txt";
+        + modelDir + "/sutime/english.holidays.sutime.txt,"
+        + modelDir + "/sutime/english.sutime.txt";
     Properties props = new Properties();
     props.setProperty("sutime.rules", sutimeRules);
     props.setProperty("sutime.binders", "0");
     pipeline.addAnnotator(new TimeAnnotator("sutime", props));
 
-    String text = "Three interesting dates are 18 Feb 1997, the 20th of july and 4 days from today.";
+    String text = "Last summer, they met every Tuesday afternoon, from 1:00 pm to 3:00 pm.";
     Annotation annotation = new Annotation(text);
     annotation.set(CoreAnnotations.DocDateAnnotation.class, SUTime.getCurrentTime().toString());
     pipeline.annotate(annotation);
     
-    System.out.println(annotation.get(CoreAnnotations.TextAnnotation.class));
+    //System.out.println(annotation.get(CoreAnnotations.TextAnnotation.class));
     
     List<CoreMap> timexAnnsAll = annotation.get(TimeAnnotations.TimexAnnotations.class);
     for (CoreMap cm : timexAnnsAll) {
